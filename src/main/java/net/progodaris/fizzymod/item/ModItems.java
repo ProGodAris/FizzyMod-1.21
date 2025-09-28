6bb072757fb9bd6e2994d31bdff307253d9faf24
ProGodAris <ngerasimo09@gmail.com>
2025-09-28 14:20:56 +0300

    Added Blahaj

--- src/main/java/net/progodaris/fizzymod/item/ModItems.java
+++ src/main/java/net/progodaris/fizzymod/item/ModItems.java
@@ -1,4 +1,22 @@
 package net.progodaris.fizzymod.item;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.progodaris.fizzymod.FizzyMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FizzyMod.MOD_ID);
+
+    public static final RegistryObject<Item> BLAHAJ = ITEMS.register("blahaj",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
 }
